fd76d6d53650ec19cf3f342aa9ebb79d0415f927
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-09-13 21:23:22 +0200

    input, output movies

--- a2-binding/src/app/app.module.ts
+++ a2-binding/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/misc/header/header.component';
 import { FilmListComponent } from './components/films/film-list/film-list.component';
 import { FilmFinderPipe } from './shared/pipes/film-finder.pipe';
+import { FilmItemComponent } from './components/films/film-item/film-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FilmListComponent,
-    FilmFinderPipe
+    FilmFinderPipe,
+    FilmItemComponent
   ],
   imports: [
     BrowserModule,
--- a2-binding/src/app/components/films/film-list/film-list.component.ts
+++ a2-binding/src/app/components/films/film-list/film-list.component.ts
@@ -12,8 +12,8 @@ export class FilmListComponent {
   posterPreview: string = '';
   searchField: string = 'title';
 
-  onClickDeleteFilm(title: string): void {
-    this.films = this.films.filter(f => f.title !== title);
+  onClickDeleteFilm(film: Film): void {
+    this.films = this.films.filter(f => f.title !== film.title)
   }
 
   ocClickCreateFilm(): void {
